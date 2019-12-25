15551fa518bb1c55517e95ce324c8bde44468cff
gitboy <1667496643@qq.com>
2019-12-25 19:22:39 +0800

    提交components

---
+++ src/components/lock1.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="templock">
+        <div class="temp_content">
+            <input type="text" id="lock_num"></br>
+            <mt-button type='default'>生成开锁码</mt-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+    #templock{
+        padding-top: 40vh;
+        text-align: center;
+        .temp_content{
+
+        }
+    }
+
+</style>
